b307f2ab69effedc00ce650e83798899c2f8571b
thak1411 <thak1411@gmail.com>
2026-07-21 00:37:31 +0900

    release: v0.6.0

--- Formula/pps.rb
+++ Formula/pps.rb
@@ -1,26 +1,26 @@
 class Pps < Formula
   desc "Official command-line interface for PROJECT-PS"
   homepage "https://github.com/PROJECT-PS/PPS-CLI"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/PROJECT-PS/PPS-CLI/releases/download/v0.5.0/pps_v0.5.0_darwin_arm64.tar.gz"
-      sha256 "56c7f72eaeb4ccc212e1fb441e31aeb8c0f2405ba54baad475eeb5934db3aa71"
+      url "https://github.com/PROJECT-PS/PPS-CLI/releases/download/v0.6.0/pps_v0.6.0_darwin_arm64.tar.gz"
+      sha256 "51d09fe4a0491049ab656c4525d2f0cbb3f9f011191eb04b537b7369e219b6d1"
     else
-      url "https://github.com/PROJECT-PS/PPS-CLI/releases/download/v0.5.0/pps_v0.5.0_darwin_amd64.tar.gz"
-      sha256 "05bbbaa0d5e2fb755733c717f613197849b92b76f58dea5327730f4830cd5d9e"
+      url "https://github.com/PROJECT-PS/PPS-CLI/releases/download/v0.6.0/pps_v0.6.0_darwin_amd64.tar.gz"
+      sha256 "7b7f0beb4d4b492a82b0a781c5ed7142705f003276ac57fbf72a18a22061dfe4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/PROJECT-PS/PPS-CLI/releases/download/v0.5.0/pps_v0.5.0_linux_arm64.tar.gz"
-      sha256 "1ba04d1346e20b144e0a134d13b70d938bc9550a6faf1c383c18a4ce798a97db"
+      url "https://github.com/PROJECT-PS/PPS-CLI/releases/download/v0.6.0/pps_v0.6.0_linux_arm64.tar.gz"
+      sha256 "ee1605c0b75be2f6843622f5a9419df48bb0cd5ee664aa6a26ee0dbf38fbe0c8"
     else
-      url "https://github.com/PROJECT-PS/PPS-CLI/releases/download/v0.5.0/pps_v0.5.0_linux_amd64.tar.gz"
-      sha256 "d26398779ba5c6d0402d806d3af5f5a5716902695cfa776411c3567caf47e99d"
+      url "https://github.com/PROJECT-PS/PPS-CLI/releases/download/v0.6.0/pps_v0.6.0_linux_amd64.tar.gz"
+      sha256 "5c092bbf9bcf6661eba998755f8ddcf97fcb8742e7199732cd68c35e2091769d"
     end
   end
 
